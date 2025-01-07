7bd2eeba96c6cb8d86f1debeb7739ada824c672d
shamimul.haque <shamimul.haque@webable.digital>
2025-01-07 12:01:07 +0600

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListing from "@/components/JobListing.vue";
+</script>
+
+<template>
+  <Navbar />
+
+  <Hero
+    title="Find Your Dream Job Here!"
+    subtitle="Find Job That Fits Your Skills and Needs!"
+  />
+  <HomeCards />
+  <JobListing />
+</template>
